1f87c6018e0f4cd04875e2a1dd418f9d27d3c900
Raymond <siaomingjeng@users.noreply.github.com>
2021-10-22 15:24:24 +1100

    Update env_prepare_general.sh

--- env_prepare_general.sh
+++ env_prepare_general.sh
@@ -2,4 +2,4 @@
 
 uname -a |grep -i el7 && NAME="env_prepare_general_centos.sh"
 uname -a |grep -i ubuntu && NAME="env_prepare_general_ubuntu.sh"
-[[ "A$NAME" == "A" ]]&& exit 1 || (echo "Run $NAME . . .!"; curl -s https://raw.githubusercontent.com/siaomingjeng/utest/main/env_prepare_general_ubuntu.sh|bash)
+[[ "A$NAME" == "A" ]]&& exit 1 || (echo "Run $NAME . . .!"; curl -s "https://raw.githubusercontent.com/siaomingjeng/utest/main/env_prepare_general_ubuntu.sh"|bash)
