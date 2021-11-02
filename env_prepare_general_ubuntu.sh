6f41438d2f7cdc357b032f5f839e8cdc85165018
Raymond <siaomingjeng@users.noreply.github.com>
2021-11-03 10:14:52 +1100

    Update env_prepare_general_ubuntu.sh

--- env_prepare_general_ubuntu.sh
+++ env_prepare_general_ubuntu.sh
@@ -3,6 +3,7 @@
 LOG(){ echo -e "\e[38;5;40m`date`: $@ \e[39m";}
 LOG "Update Package List"
 apt-get update
+sleep 60
 
 LOG "Install python-pip python-devel expect tree git jq build-essential. . ."
 apt-get install -y python3 python3-pip expect tree git jq lsb-release gnupg build-essential virtualenv
